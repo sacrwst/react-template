22afa116c7d1c4ec17cb1f269606ae7fb099b28d
sacrwst <sacrwst@gmail.com>
2023-03-17 17:57:08 +0900

    ➕ ESLint, Prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    quotes: ["error", "single"],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
